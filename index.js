c0f17bab0d403e9bfea6c70673e17dde4e4e1dcf
aneshwar <aneshwaryadavyadav@gmail.com>
2022-04-20 09:47:05 -0700

    Inital commit

---
+++ index.js
@@ -0,0 +1,34 @@
+
+
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+const port = 8080 || 3000
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+app.use(bodyParser.urlencoded())
+
+app.use(bodyParser.json())
+
+mongoose.connect("mongodb://localhost:27017/newsb", {useNewUrlParser:true});
+
+const studentSchema = new mongoose.Schema({
+    name: String,
+   age: Number,
+   subject: String,
+})
+const Student = new mongoose.model('photos', studentSchema);
+
+app.get('/', (req,res) => {
+    Student.find({}, (err, docs)=> {
+       res.json(docs)
+   })
+
+})
+
+
+
+app.listen(port, () => {
+    console.log(`concted:${port}`)
+})
